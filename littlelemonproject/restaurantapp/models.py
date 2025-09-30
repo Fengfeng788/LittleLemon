41601a4536c8788b56aa6c7f93989b7d156c6632
Fengfeng788 <751122455@qq.com>
2025-09-30 19:39:03 +0800

    message

---
+++ littlelemonproject/restaurantapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model): 
+    ID = models.IntegerField(primary_key=True,max_length=5)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(max_length=5)
+ 
+
+    def __str__(self):
+        return self.Title
+    
+class  Booking(models.Model):
+    ID = models.IntegerField(primary_key=True,max_length=11)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateField()
+    # Menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
